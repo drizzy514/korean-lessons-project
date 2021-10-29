aa6e88ccf81d01437a65cb302b47bec34ee48659
abduhakim <78894135+drizzy514@users.noreply.github.com>
2021-10-28 23:57:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter } from "react-router-dom";
-import { ContextProvider } from "./Context/context"
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ContextProvider>
           <App />
-      </ContextProvider>
-    </BrowserRouter>
+     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
